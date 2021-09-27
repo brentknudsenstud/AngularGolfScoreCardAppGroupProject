378303075eb58e782b3c93ecaf7d3cb5a8c3a3a8
Brent Knudsen <>
2021-09-27 14:57:25 -0600

    prep for production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,11 @@
 export const environment = {
+production: true,
+firebase: {
   apiKey: "AIzaSyCiAEyoxIQ5L3Oncil6NSTdCG4Yv-ojYk0",
   authDomain: "angulargolfappbrent.firebaseapp.com",
   projectId: "angulargolfappbrent",
   storageBucket: "angulargolfappbrent.appspot.com",
   messagingSenderId: "441981850242",
   appId: "1:441981850242:web:156a7eb96e1d20364d5a9b"
+}
 };
